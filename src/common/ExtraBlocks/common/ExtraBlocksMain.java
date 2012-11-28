8bd3c7d08f43cdfb164ed76325e5339de499cd40
Jack Moorhouse <moverperfect@gmail.com>
2012-11-28 17:50:38 +0000

    Updated

--- src/common/ExtraBlocks/common/ExtraBlocksMain.java
+++ src/common/ExtraBlocks/common/ExtraBlocksMain.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 
 
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
-@Mod (modid = "extraBlocksMod", name = "extraBlocksMod", version = "0.2.0")
+@Mod (modid = "extraBlocksMod", name = "extraBlocksMod", version = "0.2.1")
 
 public class ExtraBlocksMain {
 	
