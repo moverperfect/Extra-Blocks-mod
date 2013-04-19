a8dc22d2ba3b3273a55bde11c5258ed11f529b54
Jack Moorhouse <moverperfect@gmail.com>
2013-04-20 00:31:13 +0100

    Updates

--- common/com/moverperfect/ebm/core/handlers/ExtraBlocksFuelHandler.java
+++ common/com/moverperfect/ebm/core/handlers/ExtraBlocksFuelHandler.java
@@ -1,17 +1,26 @@
 package com.moverperfect.ebm.core.handlers;
 
-import com.moverperfect.ebm.ExtraBlocksMain;
+import com.moverperfect.ebm.lib.BlockIds;
 
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.IFuelHandler;
+
+/**
+ * Extra-Blocks-Mod
+ * 
+ * ExtraBlocksFuelHandler
+ * 
+ * @author Moverperfect
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
 
 public class ExtraBlocksFuelHandler implements IFuelHandler {
 	@Override
 	public int getBurnTime(ItemStack fuel) {
 		int var1 = fuel.itemID;
-		
 		// Id item in the furnace = coal block then make it last for 12800 ticks
-		if(var1 == ExtraBlocksMain.coalBlockID){
+		if(var1 == BlockIds.COAL_BLOCK){
 			return 12800;
 		}else{
 			return 0;
