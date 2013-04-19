1bfd5b1e4595ac211dc45fa876391d2f6954556c
Jack Moorhouse <moverperfect@gmail.com>
2013-04-20 00:32:57 +0100

    Added block ids

---
+++ common/com/moverperfect/ebm/lib/BlockIds.java
@@ -0,0 +1,32 @@
+package com.moverperfect.ebm.lib;
+
+/**
+ * Extra-Blocks-Mod
+ * 
+ * BlockIds
+ * 
+ * @author Moverperfect
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
+
+public class BlockIds {
+	// TODO The ID's
+    /* Default block ids */
+    public static int COPPER_ORE_DEFAULT = 700;
+    public static int COPPER_BLOCK_DEFAULT = 701;
+    public static int TIN_ORE_DEFAULT = 702;
+    public static int TIN_BLOCK_DEFAULT = 703;
+    public static int REDSTONE_BLOCK_DEFAULT = 704;
+    public static int COAL_BLOCK_DEFAULT = 705;
+       
+
+    /* Current block ids */
+    public static int COPPER_ORE;
+    public static int COPPER_BLOCK;
+    public static int TIN_ORE;
+    public static int TIN_BLOCK;
+    public static int REDSTONE_BLOCK;
+    public static int COAL_BLOCK;
+
+}
