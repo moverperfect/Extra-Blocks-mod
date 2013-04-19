cf81a07a56d7cb50f29fc8af05caeea6080a3aad
Jack Moorhouse <moverperfect@gmail.com>
2013-04-20 00:32:29 +0100

    Added licence

--- common/com/moverperfect/ebm/core/proxy/CommonProxy.java
+++ common/com/moverperfect/ebm/core/proxy/CommonProxy.java
@@ -1,9 +1,17 @@
 package com.moverperfect.ebm.core.proxy;
 
-public class CommonProxy
-{
-public void registerRenderThings()
-{
+/**
+ * Extra-Blocks-Mod
+ * 
+ * CommonProxy
+ * 
+ * @author Moverperfect
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
 
-}
+public class CommonProxy    {
+    public void registerRenderThings()  {
+        
+    }
 }
