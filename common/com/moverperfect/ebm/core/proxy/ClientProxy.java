86b55e7f1da872c496c4fc9dfc734b57e14d3fc1
Jack Moorhouse <moverperfect@gmail.com>
2013-04-20 00:34:33 +0100

    Added licensing

--- common/com/moverperfect/ebm/core/proxy/ClientProxy.java
+++ common/com/moverperfect/ebm/core/proxy/ClientProxy.java
@@ -1,12 +1,18 @@
 package com.moverperfect.ebm.core.proxy;
 
-import net.minecraftforge.client.MinecraftForgeClient;
+/**
+ * Extra-Blocks-Mod
+ * 
+ * ClientProxy
+ * 
+ * @author Moverperfect
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
 
-public class ClientProxy extends CommonProxy
-{
-@Override
-public void registerRenderThings()
-{
-MinecraftForgeClient.preloadTexture("/textures/Blocks.png");
-}
+public class ClientProxy extends CommonProxy    {
+    @Override
+    public void registerRenderThings()  {
+        
+    }
 }
